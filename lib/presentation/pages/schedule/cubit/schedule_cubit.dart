c057627aa2fdc16a8ecd82533ce8061e7c2f13d4
yngve3 <tolstov200213@gmail.com>
2024-03-22 16:25:29 +0300

    The functionality of mixing the receipt of the schedule and creating its further conversion is brought to the Interactor

--- lib/presentation/pages/schedule/cubit/schedule_cubit.dart
+++ lib/presentation/pages/schedule/cubit/schedule_cubit.dart
@@ -1,17 +1,9 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:schedule_for_ictis_flutter/data/repositories/favorite_schedules_repository.dart';
-import 'package:schedule_for_ictis_flutter/data/repositories/schedule_repository.dart';
+import 'package:schedule_for_ictis_flutter/domain/interactors/schedule_interactor.dart';
 import 'package:schedule_for_ictis_flutter/presentation/pages/schedule/cubit/schedule_state.dart';
 
-import '../../../../data/models/week_schedule_db.dart';
-import '../../../../domain/models/schedule/day_schedule/day_schedule.dart';
-import '../../../../domain/models/schedule/day_schedule_item.dart';
 import '../../../../domain/models/schedule/week_schedule/week_schedule.dart';
-import '../../../../domain/models/schedule_subject/schedule_subject.dart';
 
 class ScheduleCubit extends Cubit<ScheduleState> {
   ScheduleCubit() : super(
@@ -21,48 +13,7 @@ class ScheduleCubit extends Cubit<ScheduleState> {
       )
   );
 
-  final _scheduleRepository = ScheduleRepository();
-  final _favoriteSchedulesRepository = FavoriteSchedulesRepository();
-
-  StreamSubscription<ScheduleSubject?>? scheduleSubjectSubscription;
-
-
-  void getWeekSchedule({int? weekNum}) {
-    final scheduleSubject = _favoriteSchedulesRepository.getSelectedFavoriteSchedule();
-
-    if (scheduleSubjectSubscription != null) scheduleSubjectSubscription!.cancel();
-    scheduleSubjectSubscription = scheduleSubject.listen((event) async {
-      final schedule = await _scheduleRepository.getWeekSchedule(event!, weekNum: weekNum);
-      emit(state.copyWith(weekSchedule: generateWeekSchedule(schedule)));
-    });
-  }
-
-  WeekSchedule generateWeekSchedule(WeekScheduleDB weekScheduleDB) {
-    List<DaySchedule> daySchedules = [];
-    for (final dayScheduleDB in weekScheduleDB.daySchedules) {
-      List<DayScheduleItem> couples = [];
-      for (int i = 0; i < dayScheduleDB.couples.length; i++) {
-        final coupleDB = dayScheduleDB.couples[i];
-        couples.add(Couple.fromCoupleDB(coupleDB, i + 1));
-      }
-
-      couples.add(Event(const TimeOfDay(hour: 11, minute: 1), const TimeOfDay(hour: 2, minute: 2), title: "fk", description: "lsakdjf"));
-
-      couples.sort((a, b) => a.timeStart.compareTo(b.timeStart));
-
-      daySchedules.add(DaySchedule(items: couples));
-    }
-
-    daySchedules.add(DaySchedule(items: [Event(const TimeOfDay(hour: 1, minute: 1), const TimeOfDay(hour: 2, minute: 2), title: "fk", description: "lsakdjf")]));
-
-    return WeekSchedule(weekNum: weekScheduleDB.weekNum, daySchedules: daySchedules);
-  }
-
-  @override
-  Future<void> close() {
-    if (scheduleSubjectSubscription != null) scheduleSubjectSubscription!.cancel();
-    return super.close();
-  }
+  ScheduleInteractor interactor = ScheduleInteractor();
 
   void nextDay() {
     emit(state.copyWith(selectedDay: state.selectedDay.add(const Duration(days: 1))));
@@ -76,7 +27,7 @@ class ScheduleCubit extends Cubit<ScheduleState> {
   void selectDay(DateTime date) {
     final weekNumDifference = _weekNumDifference(date);
     if (weekNumDifference != 0) {
-      getWeekSchedule(weekNum: state.weekSchedule.weekNum + weekNumDifference);
+      interactor.getWeekSchedule(weekNum: state.weekSchedule.weekNum + weekNumDifference);
     }
     emit(state.copyWith(selectedDay: date));
   }
@@ -93,16 +44,6 @@ class ScheduleCubit extends Cubit<ScheduleState> {
 
   int _weekNumDifference(DateTime newDay) {
     return newDay.weekNumber() - state.selectedDay.weekNumber();
-  }
-}
-
-extension on TimeOfDay {
-  int compareTo(TimeOfDay other) {
-    if (hour < other.hour) return -1;
-    if (hour > other.hour) return 1;
-    if (minute < other.minute) return -1;
-    if (minute > other.minute) return 1;
-    return 0;
   }
 }
 
